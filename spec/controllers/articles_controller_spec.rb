179f859aafa65ddb5fd0d7c5e4c04bc274ac8f18
odumade <chekkitapp@gmail.com>
2022-10-09 15:09:48 +0100

    set up index test case

---
+++ spec/controllers/articles_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe ArticlesController do
+  describe '#index' do
+    it 'should return success response' do
+      get :index
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should return proper json' do
+      get :index
+      json = JSON.parse(response.body)
+      json_data = json[:data]
+      pp json 
+    end
+  end
+end

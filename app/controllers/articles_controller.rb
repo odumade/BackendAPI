0f4300579af2daf385e6446505008747af1d7b02
odumade <chekkitapp@gmail.com>
2022-10-10 09:05:14 +0100

    add pagination and order by  with test cases

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,11 @@
 class ArticlesController < ApplicationController
     def index
-        articles = Article.all
-        render json: articles
+      articles = Article.recent.
+        page(params[:page]).
+        per(params[:per_page])
+      render json: articles
     end
-
+  
     def show
     end
-end
+  end
--- spec/support/json_api_helpers.rb
+++ spec/support/json_api_helpers.rb
@@ -1,9 +1,9 @@
 module JsonApiHelpers
     def json
-        JSON.parse(response.body)
+      JSON.parse(response.body)
     end
-
+  
     def json_data
-        json["data"]
+      json["data"]
     end
-end
+  end
